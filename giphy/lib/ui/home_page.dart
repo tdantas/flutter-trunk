6df982f0314321504c4a22970dc590bbd5c207b7
Thiago Dantas <thiago.teixeira.dantas@gmail.com>
2020-01-09 23:17:12 +0000

    update giphy

--- giphy/lib/ui/home_page.dart
+++ giphy/lib/ui/home_page.dart
@@ -33,8 +33,61 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
+  Widget _createGifTable(BuildContext context, AsyncSnapshot data) {
+    return Container(color: Colors.amber);
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(color: Colors.green);
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.black,
+        centerTitle: true,
+        title: Image.network("https://developers.giphy.com/branch/master/static/header-logo-8974b8ae658f704a5b48a2d039b8ad93.gif")
+      ),
+      backgroundColor: Colors.black,
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          Padding(
+            padding: EdgeInsets.all(10),
+            child: TextField(
+              style: TextStyle(color: Colors.white, fontSize: 18),
+              decoration: InputDecoration(
+                  labelText: "Search",
+                  prefixIcon: Icon(Icons.search, color: Colors.white,),
+                  labelStyle: TextStyle(color: Colors.white),
+                  enabledBorder: OutlineInputBorder(
+                      borderSide: BorderSide(color: Colors.white),
+                      borderRadius: BorderRadius.all(Radius.circular(5))
+                  )
+              ),
+            ) ,
+          ),
+          Expanded(
+            child: FutureBuilder(
+              future: _search(),
+              builder: (context, asyncState) {
+                switch(asyncState.connectionState) {
+                  case ConnectionState.waiting:
+                  case ConnectionState.none:
+                    return Container(
+                      alignment: Alignment.center,
+                      child: CircularProgressIndicator(
+                        valueColor: AlwaysStoppedAnimation<Color>(Colors.white),
+                        strokeWidth: 5,
+                      ),
+                    );
+                  default:
+                    if(asyncState.hasError) return Container(color: Colors.pink);
+                    else return _createGifTable(context, asyncState);
+                }
+              }
+            ),
+          )
+        ],
+
+      ),
+    );
   }
 }
