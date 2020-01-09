558e6f432e543b704c47a827e083f793c9706a8a
Thiago Dantas <thiago.teixeira.dantas@gmail.com>
2020-01-09 21:47:35 +0000

    update snackbar stack

--- task_todo_list/lib/main.dart
+++ task_todo_list/lib/main.dart
@@ -63,7 +63,7 @@ class _HomeState extends State<Home> {
                     });
                   }));
 
-          //Scaffold.of(context).removeCurrentSnackBar();
+          Scaffold.of(context).removeCurrentSnackBar();
           Scaffold.of(context).showSnackBar(snack);
         });
 
